fa241f7e651585d45cb3ad08a33862d2f472f054
BryanHernandez18550679 <82691139+BryanHernandez18550679@users.noreply.github.com>
2021-04-16 21:54:54 -0600

    Add files via upload

---
+++ EVA1_1_PRIMER_APP/app/src/main/java/com/example/eva1_1_primer_app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.eva1_1_primer_app;
+/*written by: <BRYAN ERNESTO HERNANDEZ CANO>
+<18550679>
+<16/04/2021>*/
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
